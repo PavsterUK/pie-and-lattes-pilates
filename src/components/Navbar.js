e666b26d5a8835e3f5480b8e842fae984fc5b8d0
Pavel Naumovic <pavelnaumovic@gmail.com>
2022-02-09 22:19:46 +0000

    href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ import styles from "../components/Navbar.module.css"
 const Navbar = () => {
     return <div className={styles.container}>
         <ul className={styles.main_nav}>
-            <li><a href="">Home</a></li>
-            <li><a href="">About Me</a></li>
-            <li><a href="">schedule</a></li>
-            <li><a href="">courses</a></li>
-            <li><a href="">classes</a></li>
-            <li><a href="">Contact us</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">schedule</a></li>
+            <li><a href="#">courses</a></li>
+            <li><a href="#">classes</a></li>
+            <li><a href="#">Contact us</a></li>
         </ul>
     </div>
 };
