0de001d124f2121bbb02892daa0665dd9d41e766
Pavel Naumovic <pavelnaumovic@gmail.com>
2022-02-10 22:03:28 +0000

    header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import logo from "../images/svg.svg";
 import styles from "../components/Navbar.module.css"
 
 const Navbar = () => {
     return <div className={styles.container}>
+        <div className={styles.logo_title}>
+            <img src={logo} />
+            <h2>KEYNSHAM PILATES STUDIO  "PIE  &#38; LATTES" </h2>
+        </div>
+
+
         <ul className={styles.main_nav}>
             <li><a href={() => false}>Home</a></li>
-            <li><a href={() => false}>About Me</a></li>
+            <li><a href={() => false}>About</a></li>
             <li><a href={() => false}>schedule</a></li>
             <li><a href={() => false}>courses</a></li>
             <li><a href={() => false}>classes</a></li>
-            <li><a href={() => false}>Contact us</a></li>
+            <li><a href={() => false}>Contacts</a></li>
         </ul>
     </div>
 };
