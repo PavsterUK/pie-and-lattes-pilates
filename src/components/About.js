86a44a470a92128bfd1920815ce3376b8febe5c9
Pavel Naumovich <pavelnaumovic@gmail.com>
2022-02-16 22:10:20 +0000

    about

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Card from "../components/GUI/Card";
+
+const About = () => {
+  return (
+    <Card>
+      <div>
+          <h1>Hello World</h1>
+      </div>
+    </Card>
+  );
+};
+
+export default About;
